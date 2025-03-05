8300092ac9037ddec3793d76c6803dc51d383a69
Amit biswas <amitbiswas9282@gmail.com>
2025-03-05 15:35:12 +0600

    artifacts post

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DB_USER);
+
+// const uri = `mongodb+srv://history_server:HrvpJhviV5jglHRj@cluster0.yit3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yit3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const historyCollection = client.db("historyServer").collection("history");
+    const artifactCollection = client
+      .db("historyServer")
+      .collection("artifacts");
+
+    app.post("/artifacts", async (req, res) => {
+      const user = req.body;
+      console.log("product", user);
+      const result = await artifactCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // read history product
+    app.get("/history", async (req, res) => {
+      const cursor = await historyCollection.find().toArray();
+      res.send(cursor);
+    });
+
+    // see single value history product
+    app.get("/history/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await historyCollection.findOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("History Project is Running.");
+});
+
+app.listen(port, () => {
+  console.log(`History Project is port: ${port}`);
+});
