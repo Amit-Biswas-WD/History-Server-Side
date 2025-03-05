f599119408193d700bb4d6b469f716673d9d49b0
Amit biswas <amitbiswas9282@gmail.com>
2025-03-05 20:13:04 +0600

    artifacts id

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
       res.send(cursor);
     });
 
+    app.get("/artifacts/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await artifactCollection.findOne(query)
+      res.send(result)
+    })
+
     // read history data
     app.get("/history", async (req, res) => {
       const cursor = await historyCollection.find().toArray();
